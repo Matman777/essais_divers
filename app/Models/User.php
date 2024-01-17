d2f36b5e5d8a30a8aa2162c458705b35e0d90abe
Matman777 <mathias.mauer777@gmail.com>
2024-01-17 11:34:05 +0100

    controller model ok

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use App\Models\Chirp;
+
 
 class User extends Authenticatable
 {
@@ -42,4 +45,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function chirps(): HasMany {
+        
+        return $this->hasMany(Chirp::class);
+    }
 }
